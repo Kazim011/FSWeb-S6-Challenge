a782bd335eea143d44bcce80d489c6d943f11ab1
kazım <kazimnergiz123@gmail.com>
2023-01-07 22:46:12 +0300

    ilk ornek yapıldı

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {data} from './mocks/handlers';
+import Karakter from "./components/Karakter"
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +11,17 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const characters = data.map((item,index) => 
+  <Karakter key = {index} name={item.name} birthYear = {item.birth_year}/>
+);
+
+
+
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">REACT WARS</h1>
+      {characters}
     </div>
   );
 }
